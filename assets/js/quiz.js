4b910e1389f79c8a86061c8ace2fd943c8f947d7
NB1355 <nahal5513@gmail.com>
2023-02-02 20:28:34 +0000

    Add test mode funtioality

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,97 +1,230 @@
+let queryCount = 7; // number of the selected questions
+let quizTime = 45; // munits to calculate 
+var select = [];
+var list = [];
+var record = "";
+let s = 0;
 
-const start = document.getElementById("x1-button");
-// const next 
+let testMode;
 
-const lastQuestion = questions.length - 1;
 
-let currentQuestion = 1;
-let count = 0;
-let score = 0;
 
-function showQuestion() {
-    let rq = questions[currentQuestion];
+document.getElementById("x1-button").addEventListener("click", runTest);
+document.getElementById("x2-button").addEventListener("click", questionSubmit);
+document.getElementById("x3-button").addEventListener("click", showAnswers);
+document.getElementById("x4-button").addEventListener("click", showResult);
 
-    qID.innerHTML = rq.qRefID
-    qText.innerHTML = rq.qRefText;
-    A.innerHTML = rq.checkbox1;
-    B.innerHTML = rq.checkbox2;
-    C.innerHTML = rq.checkbox3;
-    D.innerHTML = rq.checkbox4;
 
-    xCorrects = rq.corrects;
-    i = xCorrects.length;
+function runTest() {
 
-    for (i = 0; i < xCorrects.length; i++) {
-        let correct = correct.charAt(i);
-        switch (correct) {
-            case "1":
-                console.log(correct);
-                break;
-            case "2":
-                console.log(correct);
-                break;
-            case "3":
-                console.log(correct);
-                break;
-            case "4":
-                console.log(correct);
-                break;
+    let testStatus = 1;
+
+
+    document.querySelector(".x-checkbox").style.display = "block";
+
+    questionsLoad();
+    // document.querySelector(".x-answers").style.display ="none";
+
+    //     while (testStatus =1){
+    //         questionShow();
+    //     } 
+    // }
+
+}
+
+
+function getMode() {
+
+    testMode = document.getElementById("x1-radio").checked;
+    
+}
+
+function questionsLoad() {
+    fetch("assets/data/000TestDataSet.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+
+            randomSelect(data);
+            list = data; //Creates Global Variable OPT: use local possible?
+            getMode();
+
+            questionShow();
+
+            console.log(" test mode:" + testMode) + "/n";
+            if (testMode == true) {
+                document.getElementById("x1-button").disabled = true;
+                document.getElementById("x2-button").disabled = false;
+                document.getElementById("x3-button").disabled = true;
+
+            } else {
+                document.getElementById("x1-button").disabled = true;
+                document.getElementById("x2-button").disabled = false;
+                document.getElementById("x3-button").disabled = false;
+
+            }
+
+        });
+
+}
+
+
+function randomSelect(data) {
+
+    const maxNum = data.questions.length;
+    while (select.length < queryCount) {
+
+        var num = Math.floor(Math.random() * maxNum) + 1;
+        if (num <= maxNum && select.includes(num) == false) {
+            select.push(num);
         }
+    }
+    info1.innerHTML = queryCount + "/" + maxNum + " random selection: " + select;
+
+}
 
 
-        // console.log(correct);
+function questionShow() {
+
+    if (s < select.length) {
+
+        let q = select[s] - 1;
+        qID.innerHTML = list.questions[q].qRefID;
+        qText.innerHTML = list.questions[q].qRefText;
+        A.innerHTML = list.questions[q].checkbox1;
+        B.innerHTML = list.questions[q].checkbox2;
+        C.innerHTML = list.questions[q].checkbox3;
+        D.innerHTML = list.questions[q].checkbox4;
+        s++
+
+        optionesTrue(q);
+
+    } else {
+        document.getElementById("x2-button").disabled = true;
+        document.getElementById("x3-button").disabled = true;
+        document.getElementById("x4-button").disabled = false;
+
     }
 }
 
 
-// start.addEventListener("click", runQuiz);
-// next.addEventListener("click", runQuiz);
+// setup correct options
 
-function runQuiz() {
+function optionesTrue(q) {
 
-    showQuestion();
-    i++
+    optionsClear();
+
+    let xCorrects = list.questions[q].corrects
+
+    console.log(xCorrects);
+
+    for (i = 0; i < xCorrects.length; i++) {
+        let correct = xCorrects.charAt(i);
+        switch (correct) {
+            case "1":
+                document.getElementById("checkbox1").checked = true;
+                break;
+            case "2":
+                document.getElementById("checkbox2").checked = true;
+                break;
+            case "3":
+                document.getElementById("checkbox3").checked = true;
+                break;
+            case "4":
+                document.getElementById("checkbox4").checked = true;
+                break;
+        }
+    }
 }
 
 
-function check() {
-    document.getElementById().checked = check;
+function showAnswers() {
+
+    let xShow = document.getElementById("x3-button").value;
+
+    if (xShow == "Show Answers") {
+        opacity = 0.5;
+        document.getElementById("x3-button").value = "Hide Answers";
+    } else {
+        opacity = 0.01;
+        document.getElementById("x3-button").value = "Show Answers";
+    }
+
+    document.getElementById("checkbox1").style.opacity = opacity;
+    document.getElementById("checkbox2").style.opacity = opacity;
+    document.getElementById("checkbox3").style.opacity = opacity;
+    document.getElementById("checkbox4").style.opacity = opacity;
+    // document.querySelector(".x-answers").style.opacity = "0.5";  TEMP ? not working
+
+
 }
 
-function uncheck() {
-    document.getElementById().checked = uncheck;
+function questionSubmit() {
+
+    checkAnswers();
+    recordText();
+
+    optionsClear();
+    questionShow();
 }
 
-// test output
-// console.log(lastQuestion);
-// console.log(option1, option2);
+function showResult() {
 
-// function showStatus() {
-//     for (let rqIndex = 0; rqIndex <= lastQuestion; rqIndex++) {
-//         console.log("zzzz");
+    document.getElementById("xQuiz").style.display = "none";
+
+    document.getElementById("x-result").style.display = "block";
+    document.getElementById("SP").innerText = document.getElementById("scorePlus").innerHTML;
+    document.getElementById("SM").innerText = document.getElementById("scoreMinus").innerHTML;
+
+    document.getElementById("x-record").innerText = record;
+
+}
+
+function checkAnswers() {
+
+    if (option1.checked == checkbox1.checked &&
+        option2.checked == checkbox2.checked &&
+        option3.checked == checkbox3.checked &&
+        option4.checked == checkbox4.checked
+    ) {
+        scorePlus();
+        pass = "Y";
+
+    } else {
+        scoreMinus();
+        pass = "N";
+    }
+}
+// record==============================
+function recordText() {
+
+    record = record + "\n" + document.getElementById("qID").innerHTML + " Pass: " + pass;
+}
+
+function scorePlus() {
+
+    let oldScore = parseInt(document.getElementById("scorePlus").innerText);
+    document.getElementById("scorePlus").innerText = ++oldScore;
+}
+
+function scoreMinus() {
+    let oldScore = parseInt(document.getElementById("scoreMinus").innerText);
+    document.getElementById("scoreMinus").innerText = ++oldScore;
+}
 
 
-//     }
-// }
 
-// function checkAnswer(answer) {
-//     if (answer == questions[currentQuestion].correct) {
-//         // answer is correct
-//         score++;
-//         // change progress color to green
-//         answerIsCorrect();
-//     } else {
-//         // answer is wrong
-//         // change progress color to red
-//         answerIsWrong();
-//     }
-//     count = 0;
-//     if (currentQuestion < lastQuestion) {
-//         runningQuestion++;
-//         showQuestion();
-//     } else {
-//         // end the quiz and show the score
-//         clearInterval(TIMER);
-//         scoreRender();
-//     }
-// }
+
+function optionsClear() {
+
+    document.getElementById("checkbox1").checked = false;
+    document.getElementById("checkbox2").checked = false;
+    document.getElementById("checkbox3").checked = false;
+    document.getElementById("checkbox4").checked = false;
+
+    document.getElementById("option1").checked = false;
+    document.getElementById("option2").checked = false;
+    document.getElementById("option3").checked = false;
+    document.getElementById("option4").checked = false;
+}
--- assets/js/xtemp.js
+++
@@ -1,2 +0,0 @@
-// document.getElementById("xTest").addEventListener("click", );
-
